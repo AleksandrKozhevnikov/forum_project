05ad4ca9d33df12b392fec78cbc09d198c6840bf
AleksandrKozhevnikov <55677148+AleksandrKozhevnikov@users.noreply.github.com>
2020-07-27 12:10:12 +0300

    add_page__floors for kvarc

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,6 +26,30 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrov-left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrov-right.png"></button>'
     });
+    $('.kvarc__page__slider').slick({
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1500,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrov-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next_promo"><img src="img/icons/arrov-right.png"></button>'
+    });
+    $('.forum__page__slider').slick({
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1500,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrov-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next_promo"><img src="img/icons/arrov-right.png"></button>'
+    });
+    $('.atoll__page__slider').slick({
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1500,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrov-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next_promo"><img src="img/icons/arrov-right.png"></button>'
+    });
   });
 
 
